9c2c9f733a705491b3339ae36672314ec9dc0d8b
Аминев Денис Салаватович <daminev@skbkontur.ru>
2018-01-10 19:39:32 +0500

    actually application

---
+++ generator/src/generator/Generator.java
@@ -0,0 +1,17 @@
+package generator;
+
+class Generator {
+
+  final int[] numbersArray;
+
+  Generator() {
+    numbersArray = new int[10];
+
+    for (int n = 0; n < numbersArray.length; n++) {
+      int rand = (int) (Math.random() * 10);
+      numbersArray[n] = rand;
+    }
+
+  }
+
+}
---
+++ generator/src/generator/Logging.java
@@ -0,0 +1,26 @@
+package generator;
+
+public class Logging {
+
+  public static void main(String args[]) {
+      for (int n = 1; n <= 5; n++) {
+        Generator books = new Generator();
+
+        int oddCounter = 0;
+
+        for (int i : books.numbersArray) {
+          if (i % 2 != 0) {
+            oddCounter++;
+          }
+        }
+
+        if (oddCounter > books.numbersArray.length / 2) {
+          System.out.println(n + " app build " + "Failed");
+        } else {
+          System.out.println(n + " app build " + "Success");
+        }
+
+      }
+  }
+
+}
